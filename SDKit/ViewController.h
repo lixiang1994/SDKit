e9b31015b8f81923d15bab63535a414a4af6eea9
GSD_iOS <gsdios@126.com>
2016-04-18 20:26:52 +0800

    Initial Commit

---
+++ SDKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SDKit
+//
+//  Created by gsd on 16/4/18.
+//  Copyright © 2016年 gsd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SDKit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SDKit
+//
+//  Created by gsd on 16/4/18.
+//  Copyright © 2016年 gsd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
